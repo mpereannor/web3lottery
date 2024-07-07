c12bf829cc6c9d32306ccaf5f364991796ab189d
mpereannor <mpereannor@gmail.com>
2024-07-07 01:30:58 +0000

    feat: setup event

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -9,18 +9,32 @@ pragma solidity ^0.8.0;
  */
 
 contract Raffle {
+    /*** Errors */
+    error Raffle__SendMoreToEnterRaffle();
+
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /** Events */
+    event RaffleEntered(address indexed player);
+
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEntered(msg.sender);
+    }
 
     function pickWinner() public {}
 
-    /** Getter Functions */   
-    function getEntranceFee() external view returns(uint256) {
+    /** Getter Functions */
+    function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
     }
 }
